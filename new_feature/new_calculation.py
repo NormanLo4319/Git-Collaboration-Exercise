9458698baf707706df5d923f965ab9d13b123a95
Norman Lo <lokman@mail.sfsu.edu>
2023-02-27 19:44:21 -0800

    adds the new calculation.py file for group project

---
+++ new_feature/new_calculation.py
@@ -0,0 +1,18 @@
+"""
+This is the py file for storing the calculation functions
+"""
+
+def sum(a:int, b:int):
+    result = a + b
+    return result
+
+
+def product(a:int, b:int):
+    result = a * b
+    return result
+
+
+def divison(a:int, b:int):
+    result = a / b
+    result = int(result)
+    return result
